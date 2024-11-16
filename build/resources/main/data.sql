5afc4772db2e3245dff0286e2a0445e86d12d567
Alexander- <Alexander-@users.noreply.github.com>
2024-11-16 04:34:37 +0300

    Delete build directory

--- build/resources/main/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO test_table (test_id, test_column, string_column) VALUES (1, 1, 'USA');
-INSERT INTO test_table (test_id, test_column, string_column) VALUES (2, 2, 'France');
-INSERT INTO test_table (test_id, test_column, string_column) VALUES (3, 3, 'Brazil');
-INSERT INTO test_table (test_id, test_column, string_column) VALUES (4, 4, 'Italy');
-INSERT INTO test_table (test_id, test_column, string_column) VALUES (5, 5, 'Canada');
